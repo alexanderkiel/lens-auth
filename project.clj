4463b2a0cb484874915b3c7a8e9acab238d5cdf1
Alexander Kiel <alexanderkiel@gmx.net>
2015-05-20 10:28:21 +0200

    Optimize Dependencies

--- project.clj
+++ project.clj
@@ -5,12 +5,16 @@
   :min-lein-version "2.0.0"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/tools.cli "0.3.1"]
-                 [prismatic/plumbing "0.4.0"]
+                 [org.clojure/tools.reader "0.9.2"]
+                 [prismatic/plumbing "0.4.3"]
                  [http-kit "2.1.18"]
-                 [ring/ring-core "1.3.2"]
-                 [bidi "1.18.10" :exclusions [ring/ring-core]]
+                 [ring/ring-core "1.3.2" :exclusions [commons-codec]]
+                 [pathetic "0.5.1"]
+                 [bidi "1.18.11" :exclusions [org.clojure/clojurescript]]
                  [liberator "0.12.2"]
+                 [com.cognitect/transit-clj "0.8.271"]
                  [io.clojure/liberator-transit "0.3.0"]]
 
   :profiles {:dev

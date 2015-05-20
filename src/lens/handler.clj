3d9349efb16fbdf40ccb9bbea9407ca5a35918f3
Alexander Kiel <alexanderkiel@gmx.net>
2015-05-20 10:58:20 +0200

    Adapt Namespace lens.handler to Newest Conventions

--- src/lens/handler.clj
+++ src/lens/handler.clj
@@ -1,6 +1,6 @@
 (ns lens.handler
   (:use plumbing.core)
-  (:require [liberator.core :refer [defresource]]
+  (:require [liberator.core :refer [resource]]
             [liberator.representation :refer [as-response]])
   (:import [java.util UUID]))
 
@@ -13,61 +13,63 @@
 
    :handle-unprocessable-entity (fnk [error] {:error error})})
 
-(defresource token [db]
-  resource-defaults
+(defn token-handler [db]
+  (resource
+    resource-defaults
 
-  :processable?
-  (fnk [[:request params]]
-    (cond
-      (or (nil? (:grant_type params))
-          (nil? (:username params))
-          (nil? (:password params)))
-      [false {:error "invalid_request"}]
+    :processable?
+    (fnk [[:request params]]
+      (cond
+        (or (nil? (:grant_type params))
+            (nil? (:username params))
+            (nil? (:password params)))
+        [false {:error "invalid_request"}]
 
-      (not= "password" (:grant_type params))
-      [false {:error "unsupported_grant_type"}]
+        (not= "password" (:grant_type params))
+        [false {:error "unsupported_grant_type"}]
 
-      :else true))
+        :else true))
 
-  :post!
-  (fnk [[:request [:params username]]]
-    (let [token (str (UUID/randomUUID))]
-      (swap! db #(assoc % token {:username username}))
-      {:token token}))
+    :post!
+    (fnk [[:request [:params username]]]
+      (let [token (str (UUID/randomUUID))]
+        (swap! db #(assoc % token {:username username}))
+        {:token token}))
 
-  :as-response
-  (fn [d ctx]
-    (-> (as-response d ctx)
-        (assoc-in [:headers "cache-control"] "no-store")
-        (assoc-in [:headers "pragma"] "no-cache")))
+    :as-response
+    (fn [d ctx]
+      (-> (as-response d ctx)
+          (assoc-in [:headers "cache-control"] "no-store")
+          (assoc-in [:headers "pragma"] "no-cache")))
 
-  :handle-ok
-  (fnk [token]
-    {:access_token token
-     :token_type "bearer"}))
+    :handle-ok
+    (fnk [token]
+      {:access_token token
+       :token_type "bearer"})))
 
-(defresource introspect [db]
-  resource-defaults
+(defn introspect-handler [db]
+  (resource
+    resource-defaults
 
-  :processable?
-  (fnk [[:request params]]
-    (cond
-      (nil? (:token params))
-      [false {:error "invalid_request"}]
+    :processable?
+    (fnk [[:request params]]
+      (cond
+        (nil? (:token params))
+        [false {:error "invalid_request"}]
 
-      :else true))
+        :else true))
 
-  :post!
-  (fnk [[:request [:params token]]]
-    (if-let [user-info (get @db token)]
-      {::resp
-       {:active true
-        :username (:username user-info)}}
-      {::resp
-       {:active false}}))
+    :post!
+    (fnk [[:request [:params token]]]
+      (if-let [user-info (get @db token)]
+        {::resp
+         {:active true
+          :username (:username user-info)}}
+        {::resp
+         {:active false}}))
 
-  :handle-ok ::resp)
+    :handle-ok ::resp))
 
 (defn handlers [db]
-  {:token (token db)
-   :introspect (introspect db)})
+  {:token (token-handler db)
+   :introspect (introspect-handler db)})

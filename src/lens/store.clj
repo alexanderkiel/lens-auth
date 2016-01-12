bb5c5f87b1d6dccaf0d604e8e0a391b8f94cf5e9
Alexander Kiel <alexanderkiel@gmx.net>
2016-01-12 10:24:38 +0100

    Add Doc to TokenStore

--- src/lens/store.clj
+++ src/lens/store.clj
@@ -1,6 +1,11 @@
 (ns lens.store
+  (:require [schema.core :refer [Str]])
   (:refer-clojure :exclude [get]))
 
+(def UserInfo
+  {:username Str})
+
 (defprotocol TokenStore
-  (put! [this token user-info])
-  (get [this token]))
+  "A simple key-value token store."
+  (put! [this token user-info] "Puts user-info under token.")
+  (get [this token] "Returns user-info of token."))

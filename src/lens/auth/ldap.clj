4207929870097254aac1c120866d59e40e095cfc
Alexander Kiel <alexanderkiel@gmx.net>
2016-03-17 17:01:41 +0100

    Validate Username Before LDAP Search
    We used the username as is in LDAP search which could lead to unhandled
    errors. I also strip NTLM and domain names now.

--- src/lens/auth/ldap.clj
+++ src/lens/auth/ldap.clj
@@ -9,21 +9,36 @@
 (def ^:private search-opts
   {:attributes [:sAMAccountName :dn] :scope :one})
 
+(defn coerce-username
+  "Tries to coerce a valid username from s.
+   Strips a possible domains and NTLM names.
+   Returns nil on invalid usernames."
+  [s]
+  (when (string? s)
+    (let [[s] (-> s str/trim str/lower-case (str/split #"@"))]
+      (when s
+        (let [[_ s] (conj (str/split s #"\\") s)]
+          (when (and s (re-matches #"[\w-]+" s))
+            s))))))
+
 (defrecord Ldap [hosts base-dn bind-dn bind-pw search-tpl conn]
   comp/Lifecycle
-  (start [_]
+  (start [ldap]
     (let [conn (ldap/connect {:host hosts :bind-dn bind-dn :password bind-pw})]
-      (Ldap. hosts base-dn bind-dn bind-pw search-tpl conn)))
+      (assoc ldap :conn conn)))
 
-  (stop [_]
-    (Ldap. hosts base-dn bind-dn bind-pw search-tpl nil))
+  (stop [ldap]
+    (assoc ldap :conn nil))
 
   Authenticator
   (check-credentials* [_ username password]
-    (let [opts (assoc search-opts :filter (format search-tpl username))
-          user (first (ldap/search conn base-dn opts))]
-      (when (and user (ldap/bind? conn (:dn user) password))
-        {:username (:sAMAccountName user)})))
+    (when conn
+      (when-let [username (coerce-username username)]
+        (let [opts (assoc search-opts :filter (format search-tpl username))
+              [user & others] (ldap/search conn base-dn opts)]
+          (when (empty? others)
+            (when (and user (ldap/bind? conn (:dn user) password))
+              {:username (:sAMAccountName user)}))))))
 
   Descriptive
   (describe [_]
--- test/lens/auth/ldap_test.clj
+++ test/lens/auth/ldap_test.clj
@@ -8,3 +8,21 @@
     "h0,h1" ["h0" "h1"]
     "h0 h1" ["h0" "h1"]
     "h0, h1" ["h0" "h1"]))
+
+(deftest coerce-username-test
+  (is (nil? (coerce-username nil)))
+  (is (nil? (coerce-username "")))
+  (is (nil? (coerce-username "@")))
+  (is (nil? (coerce-username "\\")))
+  (is (nil? (coerce-username "*")))
+  (is (= "a" (coerce-username "a")))
+  (is (= "1" (coerce-username "1")))
+  (is (= "-" (coerce-username "-")))
+  (is (= "_" (coerce-username "_")))
+  (is (= "aa" (coerce-username "aa")))
+  (testing "strips domain names"
+    (is (= "a" (coerce-username "a@")))
+    (is (= "a" (coerce-username "a@x"))))
+  (testing "strips NTLM names"
+    (is (= "a" (coerce-username "\\a")))
+    (is (= "a" (coerce-username "x\\a")))))
